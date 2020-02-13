649a18c82ba0ebf46b4477ae01dc161eb8b9b3d3
ConnorHatch832 <56134968+ConnorHatch832@users.noreply.github.com>
2020-02-13 13:49:54 -0500

    DB for Patient
    Added Prescription classes
    Added Prescription
    Added PatientPrescription tables.

---
+++ DataMedexMVC/dbo/Tables/Prescription.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[prescription] (
+    [prescriptionId]     INT           NOT NULL IDENTITY,
+    [prescriptionName]   VARCHAR (250) NULL,
+    [prescriptionDosage] VARCHAR (250) NULL,
+    [classFID]           INT           NULL,
+    PRIMARY KEY CLUSTERED ([prescriptionId] ASC), 
+    CONSTRAINT [FK_prescription_ToTable] FOREIGN KEY ([classFID]) REFERENCES [prescriptionClasses]([classId])
+);
---
+++ DataMedexMVC/dbo/Tables/patientPrescription.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[patientPrescription] (
+    [Id]               INT           NOT NULL IDENTITY,
+    [patientFID]       INT           NULL,
+    [prescriptionFID]  INT           NULL,
+    [fulfilled]        INT           NULL,
+    [rxName]           VARCHAR (250) NULL,
+    [rxDosage]         VARCHAR (250) NULL,
+    [rxPillCount]      INT           NULL,
+    [rxNumberofDays]   INT           NULL,
+    [dateShipped]      VARCHAR (250) NULL,
+    [prescriptionDesc] VARCHAR (250) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_patientPrescription_ToTable] FOREIGN KEY ([patientFID]) REFERENCES [dbo].[Patient] ([Id]),
+    CONSTRAINT [FK_patientPrescription_ToTable_1] FOREIGN KEY ([prescriptionFID]) REFERENCES [dbo].[prescription] ([prescriptionId])
+);
+
